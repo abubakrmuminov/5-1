b01007f83bcee0f30188b7f352a7a306330b1192
Abubakr <170409v@gmail.com>
2025-05-24 15:51:10 +0500

    initial comit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+js.addEventListener("click", () => {
+    lang.src = ("../images/logos/js.png")
+})
+
+console.log(lang)
+
+cc.addEventListener("click", () => {
+    lang.src = ("../images/logos/c#.png")
+})
+
+ccc.addEventListener("click", () => {
+    lang.src = ("../images/logos/c++.png")
+})
+
+go.addEventListener("click", () => {
+    lang.src = ("../images/logos/go.png")
+})
+
+java.addEventListener("click", () => {
+    lang.src = ("../images/logos/java.png")
+})
+
+kotlin.addEventListener("click", () => {
+    lang.src = ("../images/logos/kotlin.png")
+})
+
+php.addEventListener("click", () => {
+    lang.src = ("../images/logos/php.svg")
+})
+
+py.addEventListener("click", () => {
+    lang.src = ("../images/logos/py.png")
+})
+
+ruby.addEventListener("click", () => {
+    lang.src = ("../images/logos/ruby.png")
+})
+
+rust.addEventListener("click", () => {
+    lang.src = ("../images/logos/rust.png")
+})
